5799d418dd4bdd88210cb6d2c3dab54effac3378
sethvincent <sethvincent@gmail.com>
2015-10-24 20:59:10 -0700

    v1.0.0 – accept array of strings

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 exports.toArray = function (obj) {
-  var obj = obj || {};
-  var result = [];
+  obj = obj || {}
+  var result = []
 
   Object.keys(obj).forEach(function (key, i) {
-    result.push({ key: key, value: obj[key] });
-  });
+    result.push({ key: key, value: obj[key] })
+  })
 
-  return result;
-};
+  return result
+}
 
 exports.toObject = function (arr) {
-  var arr = arr || [];
-  var result = {};
+  arr = arr || []
+  var result = {}
 
   arr.forEach(function (obj, i) {
-    result[obj.key] = obj.value;
-  });
+    if (typeof obj === 'object' && obj.key && obj.value) {
+      result[obj.key] = obj.value
+    } else {
+      result[String(arr.indexOf(obj))] = obj
+    }
+  })
 
-  return result;
+  return result
 }
